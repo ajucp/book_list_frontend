4e4945dee71ababbf992d7da4bfae6fdc58a9eff
ajucp <ajmalcp4@gmail.com>
2024-11-15 03:00:27 +0530

    edit the package.json file

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser=require('body-parser');
 const cors = require('cors');
 const {connectDB}  = require('./util/database');
 const bookRoutes=require('./routes/books');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', bookRoutes);  // All book routes prefixed with /api
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
